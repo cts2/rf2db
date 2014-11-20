b13cd5db64f68dcc1ce7ff3681ce59ff89bef9eb
hsolbrig <solbrig.harold@mayo.edu>
2014-11-20 08:17:11 -0600

    Re-jiggered to support the new Oracle MySQL connector in anticipation of running with Python 3.x.  Added CTS2 Changeset support (changeseturi and locked) to all files.   Added ModuleDependenceyFile.  Fixed a number of bugs in the SCTID_generator

--- rf2db/constants/RF2ValueSets.py
+++ rf2db/constants/RF2ValueSets.py
@@ -34,45 +34,52 @@ from rf2db.utils.sctid import sctid
 # and Extension identifiers
 
 # The root concept for all things SNOMED CT
-sctRoot         = sctid(138875005)
+sctRoot = sctid(138875005)
 
 # The root concept for all predicates
 linkage_concept = sctid(106237007)
 
 # sct_concept.definitionStatusId
 primitive = sctid(900000000000074008)
-defined   = sctid(900000000000073002)
+defined = sctid(900000000000073002)
 
 
 # sct_description.typeId
 definition = sctid(900000000000550004)
-fsn        = sctid(900000000000003001)
-synonym    = sctid(900000000000013009)
+fsn = sctid(900000000000003001)
+synonym = sctid(900000000000013009)
 
+# sct_description.caseSignificanceId
+insensitive = 900000000000448009
+sensitive = 900000000000017005
+initialChar = 900000000000020002
 
 # sct_language.acceptabilityId
-preferred  = sctid(900000000000548007)
+preferred = sctid(900000000000548007)
 acceptable = sctid(900000000000549004)
 
 # sct_language.refsetId
 us_english = sctid(900000000000509007)
 gb_english = sctid(900000000000508004)
-spanish    = sctid(450828004)
+spanish = sctid(450828004)
 
 # sct_relationship.characteristicTypeId
-some       = sctid(900000000000451002)
-all_       = sctid(900000000000452009)
+some = sctid(900000000000451002)
+all_ = sctid(900000000000452009)
 
 # sct subsumption relationship
-is_a       = sctid(116680003)
+is_a = sctid(116680003)
 
 # Root of all modules
-module     = sctid(900000000000443000)
+module = sctid(900000000000443000)
+
+# Module dependency refset
+moduleDependencyRefset = sctid(900000000000534007)
 
 # Extension identifiers - there is no SNOMED CT concept code for these
-snomedCTInternational   = sctid(900000000000207008)
-snomedCTSpanishEdition  = sctid(450829007)
-snomedCTUSExtension     = sctid(731000124108)
+snomedCTInternational = sctid(900000000000207008)
+snomedCTSpanishEdition = sctid(450829007)
+snomedCTUSExtension = sctid(731000124108)
 
 # Simple map refsets from SCT International
 icdoSimpleMap = sctid(446608001)
@@ -80,15 +87,17 @@ ctv3simplemap = sctid(900000000000497000)
 snomedrtSimpleMap = sctid(900000000000498005)
 
 # Descendants of the model component module (900000000000443000)
-snomedCTtoICD9CMequivalencyMappingModule    = sctid(449079008)
-snomedCTtoICD10ruleBasedmappingModule       = sctid(449080006)
-ihtsdoMaintainedModule                      = sctid(900000000000445007)
-snomedCTSpanishEditionModule                = sctid(449081005)
+snomedCTtoICD9CMequivalencyMappingModule = sctid(449079008)
+snomedCTtoICD10ruleBasedmappingModule = sctid(449080006)
+ihtsdoMaintainedModule = sctid(900000000000445007)
+snomedCTSpanishEditionModule = sctid(449081005)
 moduloDeLaExtensionDeLaLenguaCastellanaParaLatinoameric = sctid(450829007)
 usNationalLibraryOfMedicineMaintainedModule = sctid(731000124108)
-snomedCTcore                                = sctid(900000000000207008)
-snomedCTtoICD10CMruleBasedMappingModule     =  sctid(5991000124107)
-snomedCTmodelComponent                      = sctid(900000000000012004)
+snomedCTcore = sctid(900000000000207008)
+snomedCTtoICD10CMruleBasedMappingModule =  sctid(5991000124107)
+snomedCTmodelComponent = sctid(900000000000012004)
+cimiModule = sctid(11000160102)
+mayoModule = sctid(11000134103)
 
 # Map from module to extension identifier
 moduleToExtensionMap = {
@@ -103,9 +112,9 @@ moduleToExtensionMap = {
 
 
 # relationship characteristic type
-characteristicType      = sctid(900000000000449001)
+characteristicType = sctid(900000000000449001)
 definingRelationship = sctid(900000000000006009)   # (superclass - not actually used)
-inferredRelationship    = sctid(900000000000011006)   # in relationships file
-statedRelationship      = sctid(900000000000010007)   # in stated relationships file
+inferredRelationship = sctid(900000000000011006)   # in relationships file
+statedRelationship = sctid(900000000000010007)   # in stated relationships file
 additionalRelationship = sctid(900000000000227009)   # (No examples)
 qualifyingRelationship = sctid(900000000000225001)   # in relationships file
